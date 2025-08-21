57ca4281e9f148587ae29e789a74e17afa90db4c
Vladi Khan <vladikhan@Vladis-Laptop.local>
2025-08-21 17:55:59 +0900

    Fixed staff_members. All Good

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -1,75 +1,62 @@
 class Admin::StaffMembersController < Admin::Base
   layout "admin"
-    before_action :authorize
 
-    def index
-      unless current_admin_member
-        redirect_to admin_login_path
-      end
-      @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
-    end
+  # index, show, new, edit, create, update, destroy
+  # авторизация уже выполняется в Admin::Base, дублировать не нужно
 
-    def show
-      staff_member = StaffMember.find(params[:id])
-      redirect_to [:edit, :admin, staff_member]
-    end
-
-    def new
-      @staff_member = StaffMember.new
-    end
-
-    def edit
-      @staff_member = StaffMember.find(params[:id])
-    end
-
-    def create
-  @staff_member = StaffMember.new(staff_member_params)
-      if @staff_member.save 
-        redirect_to admin_staff_members_url, notice: "職員を登録しました"
-      else 
-        render :new
-      end
-
-  if @staff_member.save
-    flash[:notice] = "職員アカウントを作成しました"
-    redirect_to admin_staff_members_path
-  else
-    # Показываем ошибки на форме
-    flash.now[:alert] = @staff_member.errors.full_messages.join(", ")
-    Rails.logger.debug(@staff_member.errors.full_messages)  # Логи для дебага
-    render :new
+  def index
+    @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
   end
-    end
 
-    def update
-      @staff_member = StaffMember.find(params[:id])
-      if @staff_member.update(staff_member_params)
-        flash.notice = "職員アカウントを更新しました"
-        redirect_to admin_staff_members_path
-      else
-        render :edit
-      end
-    end
+  def show
+    staff_member = StaffMember.find(params[:id])
+    redirect_to [:edit, :admin, staff_member]
+  end
 
-    def destroy
-      @staff_member = StaffMember.find(params[:id])
-      @staff_member.destroy!
-      flash.notice = "職員を削除しました"
+  def new
+    @staff_member = StaffMember.new
+  end
+
+  def edit
+    @staff_member = StaffMember.find(params[:id])
+  end
+
+  def create
+    @staff_member = StaffMember.new(staff_member_params)
+    if @staff_member.save
+      flash[:notice] = "職員を登録しました"
       redirect_to admin_staff_members_path
+    else
+      flash.now[:alert] = @staff_member.errors.full_messages.join(", ")
+      render :new
     end
+  end
 
-    private def authorize
-      unless current_admin_member
-        flash.notice = "管理者(かんりしゃ)としてログインしてください"
-        redirect_to admin_login_path
-      end
+  def update
+    @staff_member = StaffMember.find(params[:id])
+    if @staff_member.update(staff_member_params)
+      flash[:notice] = "職員アカウントを更新しました"
+      redirect_to admin_staff_members_path
+    else
+      flash.now[:alert] = @staff_member.errors.full_messages.join(", ")
+      render :edit
     end
+  end
 
-    def staff_member_params
-      params.require(:staff_member).permit(
-        :email, :password, :family_name,
-        :family_name_kana, :given_name_kana,
-        :start_date, :end_date, :suspended
-      )
-    end
+  def destroy
+    @staff_member = StaffMember.find(params[:id])
+    @staff_member.destroy!
+    flash[:notice] = "職員を削除しました"
+    redirect_to admin_staff_members_path
+  end
+
+  private
+
+  def staff_member_params
+    params.require(:staff_member).permit(
+      :email, :password, :family_name,
+      :family_name_kana, :given_name_kana,
+      :start_date, :end_date, :suspended
+    )
+  end
 end
--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,9 +1,12 @@
 class StaffMemberPresenter < ModelPresenter
   delegate :suspended?, to: :object
-  #...
-  #...
-  #...
+
+  # Показывает галочку, если сотрудник приостановлен
   def suspended_mark
-  suspended? ? raw("&#x2611;") : raw("&#x2610;")
+    if object.nil?
+      raw("&#x2610;") # пустой квадрат, если объекта нет
+    else
+      suspended? ? raw("&#x2611;") : raw("&#x2610;")
+    end
   end
 end
