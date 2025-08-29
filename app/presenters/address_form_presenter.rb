7c118803094c6a8ac39e62afc984baff8a88d584
Vladi Khan <vladikhan@Vladis-Laptop.local>
2025-08-29 14:34:01 +0900

    changed styles AllGood

--- app/presenters/address_form_presenter.rb
+++ app/presenters/address_form_presenter.rb
@@ -1,5 +1,5 @@
 class AddressFormPresenter < FormPresenter
-  def postal_code_block(name, label_text, options)
+  def postal_code_block(name, label_text, options = {})
     markup(:div, class: "input-block") do |m|
       m << decorated_label(name, label_text, options)
       m << text_field(name, options)
