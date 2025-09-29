dd192433df086375a2f883a1d8fab1ec988b6a7c
Vladi Khan <vladikhan@Vladis-Laptop.local>
2025-09-29 17:21:46 +0900

    0 errors in reqAdmin

--- spec/requests/admin/top_spec.rb
+++ spec/requests/admin/top_spec.rb
@@ -1,9 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Admin::Tops", type: :request do
-  describe "GET /index" do
-    before { host! 'staff.baukis2.example.com' }
+  let(:admin_member) { create(:admin_member, password: "pw") }
 
+  before do
+    # Авторизация администратора
+    post admin_login_path, params: {
+      admin_login_form: {
+        email: admin_member.email,
+        password: "pw"
+      }
+    }
+  end
+
+  describe "GET /index" do
     it "returns http success" do
       get admin_root_path
       expect(response).to have_http_status(:success)
